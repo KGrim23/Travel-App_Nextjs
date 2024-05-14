0bf6d14122b904a662c1016037f359721ee4f1f5
KGrim23 <kalaya.grimshaw@gmail.com>
2024-05-14 23:08:20 +0100

    use props for button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,30 +4,14 @@ import { Button } from "@headlessui/react";
 type ButtonProps = {
     type: 'button' | 'submit';
     title: string;
-    icon?: string;
     variant: string;
-    full?: boolean;
-
 }
 
-
-function Example() {
+function Example({ type, title, variant }: ButtonProps) {
   return (
-    <div className="flexCenter justify-center flex flex-col gap-5 w-full mx-auto p-5">
-      <Button className="rounded text-center bg-black py-2 px-4 text-lg lg:w-96 lg:h-12 w-full h-14 text-white data-[hover]:bg-gray-500 data-[active]:bg-black-700">
-      Help Me Choose
-      </Button>
-      <Button className="rounded text-center bg-black py-2 px-4 text-lg lg:w-96 lg:h-12 w-full h-14 text-white data-[hover]:bg-gray-500 data-[active]:bg-black-700">
-      Select a Skill
-      </Button>
-      <p>OR</p>
-      <Button className="rounded flex flexCenter gap-2 underline text-center py-2 px-2 text-lg lg:w-96 lg:h-12 w-full h-14 text-black data-[hover]:bg-gray-500 data-[active]:bg-black-700">
-        <svg className="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8.032 12 1.984 1.984 4.96-4.96m4.55 5.272.893-.893a1.984 1.984 0 0 0 0-2.806l-.893-.893a1.984 1.984 0 0 1-.581-1.403V7.04a1.984 1.984 0 0 0-1.984-1.984h-1.262a1.983 1.983 0 0 1-1.403-.581l-.893-.893a1.984 1.984 0 0 0-2.806 0l-.893.893a1.984 1.984 0 0 1-1.403.581H7.04A1.984 1.984 0 0 0 5.055 7.04v1.262c0 .527-.209 1.031-.581 1.403l-.893.893a1.984 1.984 0 0 0 0 2.806l.893.893c.372.372.581.876.581 1.403v1.262a1.984 1.984 0 0 0 1.984 1.984h1.262c.527 0 1.031.209 1.403.581l.893.893a1.984 1.984 0 0 0 2.806 0l.893-.893a1.985 1.985 0 0 1 1.403-.581h1.262a1.984 1.984 0 0 0 1.984-1.984V15.7c0-.527.209-1.031.581-1.403Z"/>
-        </svg>Become a Mentor
-      </Button>
-
-    </div>
+    <button type={type} className="rounded flex flexCenter gap-2 text-center py-2 px-2 text-lg lg:w-96 lg:h-12 w-full h-14 text-white bg-black data-[hover]:bg-gray-500 data-[active]:bg-black-700">
+    <label>{title}</label>
+    </button>
   )
 }
 
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,65 +1,23 @@
-// const PrivacyPolicy = async () => {
-//   return (
-//       <div className="flex flex-col space-y-4 w-full max-w-7xl mx-auto px-4 py-8">
-//           <h1 className="text-5xl font-bold text-gray-800">Privacy Policy</h1>
-//           <h2 className="text-3xl py-5">Privacy Policy for ADPlist</h2>
-//           <p className="text-gray-700">At ADPlist Pte. Ltd., accessible from https://www.aplentify.org/, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by ADPlist Community and how we use it.</p>
-//           <p className="text-gray-700">If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.</p>
 
-//           <h3 className="text-2xl">Log Files</h3>
-//           <p className="text-gray-700">ADPlist Pte. Ltd. follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.</p>
-          
-
-//           <h3 className="text-2xl">Cookies and Web Beacons</h3>
-//           <p className="text-gray-700">Like any other website, ADPlist Pte. Ltd. uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.</p>
-
-//           <h3 className="text-2xl">Google DoubleClick DART Cookie</h3>
-//           <p className="text-gray-700">Google is one of a third-party vendor on our site. It also uses cookies, known as DART cookies, to serve ads to our site visitors based upon their visit to www.website.com and other sites on the internet. However, visitors may choose to decline the use of DART cookies by visiting the Google ad and content network Privacy Policy at the following URL: https://policies.google.com/technologies/ads</p>
-//           <p className="text-gray-700">Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on ADPlist Community, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
-//           <p className="text-gray-700">Note that ADPlist Community has no access to or control over these cookies that are used by third-party advertisers.</p>
-
-
-//           <h3 className="text-2xl">Google DoubleClick DART Cookie</h3>
-//           <p className="text-gray-700">Google is one of a third-party vendor on our site. It also uses cookies, known as DART cookies, to serve ads to our site visitors based upon their visit to www.website.com and other sites on the internet. However, visitors may choose to decline the use of DART cookies by visiting the Google ad and content network Privacy Policy at the following URL: https://policies.google.com/technologies/ads</p>
-//           <p className="text-gray-700">Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on ADPlist Community, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
-//           <p className="text-gray-700">Note that ADPlist Community has no access to or control over these cookies that are used by third-party advertisers.</p>
-
-
-//           <h3 className="text-2xl">Google DoubleClick DART Cookie</h3>
-//           <p className="text-gray-700">Google is one of a third-party vendor on our site. It also uses cookies, known as DART cookies, to serve ads to our site visitors based upon their visit to www.website.com and other sites on the internet. However, visitors may choose to decline the use of DART cookies by visiting the Google ad and content network Privacy Policy at the following URL: https://policies.google.com/technologies/ads</p>
-//           <p className="text-gray-700">Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on ADPlist Community, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
-//           <p className="text-gray-700">Note that ADPlist Community has no access to or control over these cookies that are used by third-party advertisers.</p>
-
-//           <h3 className="text-2xl">Google DoubleClick DART Cookie</h3>
-//           <p className="text-gray-700">Google is one of a third-party vendor on our site. It also uses cookies, known as DART cookies, to serve ads to our site visitors based upon their visit to www.website.com and other sites on the internet. However, visitors may choose to decline the use of DART cookies by visiting the Google ad and content network Privacy Policy at the following URL: https://policies.google.com/technologies/ads</p>
-//           <p className="text-gray-700">Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on ADPlist Community, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
-//           <p className="text-gray-700">Note that ADPlist Community has no access to or control over these cookies that are used by third-party advertisers.</p>
-
-//           <h3 className="text-2xl">Google DoubleClick DART Cookie</h3>
-//           <p className="text-gray-700">Google is one of a third-party vendor on our site. It also uses cookies, known as DART cookies, to serve ads to our site visitors based upon their visit to www.website.com and other sites on the internet. However, visitors may choose to decline the use of DART cookies by visiting the Google ad and content network Privacy Policy at the following URL: https://policies.google.com/technologies/ads</p>
-//           <p className="text-gray-700">Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on ADPlist Community, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
-//           <p className="text-gray-700">Note that ADPlist Community has no access to or control over these cookies that are used by third-party advertisers.</p>
-          
-//       </div>
-
-//   )
-// }
-
-// export default PrivacyPolicy
-
-
-
+import { Button } from "@headlessui/react"
 import Image from "next/image"
-// import Button from "./Button"
+import Example from "./Button"
 
 const Hero = () => {
   return (
     <section className="flex flex-col pt-8 p-8 max-container padding-container lg:gap-5 lg:pt-8">
       <h1 className="bold-52 text-md lg:text-2xl lg:bold-88 text-center">Aplentify your skills & career growth</h1>
-      <p className="regular-20 text-center text-black pt-8">Get instant helgp from our friendly <span className="font-extrabold">Appie</span> AI assistant and connect with global mentors, tutors, and coaching experts!
+      <p className="regular-20 text-center text-black pt-8">Get instant help from our friendly <span className="font-extrabold">Appie</span> AI assistant and connect with global mentors, tutors, and coaching experts!
       </p>
-    
+
+      <div className="flexCenter justify-center flex flex-col gap-5 w-full mx-auto p-5">
+        <Example type="button" title="Help Me Choose" variant="" />
+        <Example type="button" title="Select a skill" variant="" />
+        <Example type="button" title="Become a Mentor" variant="btn_dark_outline" />
+    </div>
     </section>
+
+
     // <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
     //   <div className="hero-map" />
 
@@ -82,7 +40,6 @@ const Hero = () => {
     //     <div className="flex flex-col w-full gap-3 sm:flex-row">
     //       <Button type="button" title="Download App" variant="btn_green"/>
     //       <Button type="button" title="How we work?" icon="/play.svg" variant="btn_white_text"/>
-
     //     </div>
     //   </div>
 
